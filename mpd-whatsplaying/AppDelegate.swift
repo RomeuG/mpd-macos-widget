4cf17fe7a566979b36ea9aa1527d535db5bb3db8
Romeu Gomes <romeu.bizz@gmail.com>
2019-09-08 23:17:20 +0100

    Initial Commit

---
+++ mpd-whatsplaying/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mpd-whatsplaying
+//
+//  Created by Romeu Vieira da Silva Gomes on 08/09/2019.
+//  Copyright © 2019 Romeu Vieira da Silva Gomes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mpd-whatsplaying/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mpd-whatsplaying
+//
+//  Created by Romeu Vieira da Silva Gomes on 08/09/2019.
+//  Copyright © 2019 Romeu Vieira da Silva Gomes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
