b60213931ad41bc3049311107de172661a9e9188
Romeu Gomes <romeu.bizz@gmail.com>
2019-09-12 23:39:39 +0100

    Initialize TCP client and connect to default ip/port

--- mpd-whatsplaing-extension/TodayViewController.swift
+++ mpd-whatsplaing-extension/TodayViewController.swift
@@ -11,32 +11,44 @@ import NotificationCenter
 import SwiftSocket
 
 class TodayViewController: NSViewController, NCWidgetProviding {
-
+    
     @IBOutlet weak var btnPlayPause: NSButton!
     @IBOutlet weak var btnNext: NSButton!
     @IBOutlet weak var btnPrevious: NSButton!
     
+    var tcpClient: TCPClient? = nil;
+    
     let ipAddress: String = "127.0.0.1"
-    let defaultPort: String = "6600"
+    let defaultPort: Int32 = 6600
     
     override var nibName: NSNib.Name? {
         return NSNib.Name("TodayViewController")
     }
-
+    
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Update your data and prepare for a snapshot. Call completion handler when you are done
         // with NoData if nothing has changed or NewData if there is new data since the last
         // time we called you
         completionHandler(.noData)
     }
-
+    
     @IBAction func btnPreviousAction(_ sender: Any) {
         print("test")
+        //let result = tcpClient?.send(data: data)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //let client = TCPClient(address: "www.apple.com", port: 80)
+        tcpClient = TCPClient(address: ipAddress, port: defaultPort)
+        
+        switch tcpClient?.connect(timeout: 10) {
+        case .success:
+            print("Success!")
+        case .failure(let error):
+            print("Failure: \(error.localizedDescription)")
+        case .none:
+            print("None!")
+        }
     }
 }
